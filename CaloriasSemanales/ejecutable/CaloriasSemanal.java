a82b366e45c70d1969404504fea961054dcf421e
JorgeM28 <jorge2224764@correo.uis.edu.co>
2023-03-02 06:25:17 -0500

    Trabajo

--- CaloriasSemanales/ejecutable/CaloriasSemanal.java
+++ CaloriasSemanales/ejecutable/CaloriasSemanal.java
@@ -6,18 +6,20 @@ public class CaloriasSemanal
 {
     public static void main(String[] args) 
     {
-        int calorias[], prom=0, max=0, min=0, promfinal, a=0, b=0;
+        int calorias[], Total=0, max=0, min=0, promfinal, a=0, b=0;
         calorias = new int[7];
         String[] dias = {"Lunes","Martes","Miercoles","Jueves","Viernes","Sabado","Domingo"};
 
-        JOptionPane.showMessageDialog(null, "Bienvenido a tu contador de calorias semanal:)\nPor favor digita los datos necesarios para poder darte un buen resultado. ");
+        JOptionPane.showMessageDialog(null, "Bienvenido a tu contador de calorias semanal:)\nPor favor digita los datos necesarios para poder darte un buen resultado. ",
+        "BIENVENIDO A TU APP DE CALORIAS SEMANAL",
+        JOptionPane.PLAIN_MESSAGE);
 
         for(int i=0;i<calorias.length;i++)
         {
             calorias[i]=Integer.parseInt(JOptionPane.showInputDialog(null, "Ingrese las calorias del dia "+ dias[i]));
-            prom=calorias[i]+prom;  
+            Total=calorias[i]+Total;  
         }
-        promfinal=prom/7;
+        promfinal=Total/7;
         for (int i=0; i<calorias.length; i++)
         {
             if (calorias[i]>max)
@@ -35,12 +37,22 @@ public class CaloriasSemanal
                 b=i;
             }
         }
-        JOptionPane.showMessageDialog(null,"Las calorias consumidas en la semana fueron: "+ Arrays.toString(calorias));
-        JOptionPane.showMessageDialog(null," Promedio de calorias de la semana: "+ promfinal + " calorias " );
-        JOptionPane.showMessageDialog(null," El dia con mas calorias en la semana fue el "+dias[a]+" con "+max+" calorias" );
-        JOptionPane.showMessageDialog(null," El dia con menos calorias en la semana fue el "+dias[b]+" con "+min+" calorias" );
+        JOptionPane.showMessageDialog(null,"Las calorias consumidas en la semana fueron: "+ Arrays.toString(calorias),
+        "CALORIAS DE LA SEMANA",
+        JOptionPane.PLAIN_MESSAGE);
+        JOptionPane.showMessageDialog(null," Promedio de calorias de la semana: "+ promfinal + " calorias ",
+        "PROMEDIO",
+        JOptionPane.PLAIN_MESSAGE );
+        JOptionPane.showMessageDialog(null," El dia con mas calorias en la semana fue el "+dias[a]+" con "+max+" calorias",
+        "DIA CON MAYOR CANTIDAD DE CALORIAS",
+        JOptionPane.PLAIN_MESSAGE );
+        JOptionPane.showMessageDialog(null," El dia con menos calorias en la semana fue el "+dias[b]+" con "+min+" calorias",
+        "DIA CON MENOR CANTIDAD DE CALORIAS",
+        JOptionPane.PLAIN_MESSAGE);
         Arrays.sort(calorias);
-        JOptionPane.showMessageDialog(null,"Calorias de la semana en forma ordenada: "+Arrays.toString(calorias));
+        JOptionPane.showMessageDialog(null,"Calorias de la semana en forma ordenada: "+Arrays.toString(calorias),
+        "CALORIAS DE MENOR A MAYOR",
+        JOptionPane.PLAIN_MESSAGE);
 
         System.exit(0);
     }
